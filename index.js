44e4c70545b2aaeefb2e05af087502e72a92d64f
andrei <andre4acc@gmail.com>
2022-05-16 23:21:48 +0700

    added

--- index.js
+++ index.js
@@ -2,21 +2,30 @@ require('dotenv').config();
 const express = require('express')
 const App = express();
 const cookieParser = require('cookie-parser')
-const db = require('./src/config/db')
+const {connection} = require('./src/config/db')
 const port = 8000
-
-    db.connect().then(()=>{
-        const mainRouter = require('./src/routes/index') 
-        console.log(`Database connected`);
-        App.use(express.static('public'))
-        App.use(express.json())
-        App.use(express.urlencoded({extended:false}))
-        App.use(cookieParser())
-        App.use(mainRouter)
-        App.listen(port, ()=>{
-            console.log(`Connected at port ${port}`);
-        })
-
-    }).catch((err)=>{
-        console.log(err);
+connection()
+const mainRouter = require('./src/routes/index') 
+    App.use(express.static('public'))
+    App.use(express.json())
+    App.use(express.urlencoded({extended:false}))
+    App.use(cookieParser())
+    App.use(mainRouter)
+    App.listen(port, ()=>{
+        console.log(`Connected at port ${port}`);
     })
+    // db.connect().then(()=>{
+    //     const mainRouter = require('./src/routes/index') 
+    //     console.log(`Database connected`);
+    //     App.use(express.static('public'))
+    //     App.use(express.json())
+    //     App.use(express.urlencoded({extended:false}))
+    //     App.use(cookieParser())
+    //     App.use(mainRouter)
+    //     App.listen(port, ()=>{
+    //         console.log(`Connected at port ${port}`);
+    //     })
+
+    // }).catch((err)=>{
+    //     console.log(err);
+    // })
--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,24 @@ const psql = require('pg')
 const {Pool} = psql
 
 const db = new Pool({
+    // eslint-disable-next-line no-undef
     user : process.env.DB_USER,
+    // eslint-disable-next-line no-undef
     host : process.env.DB_HOST,
+    // eslint-disable-next-line no-undef
     database : process.env.DB_DATABASE,
+    // eslint-disable-next-line no-undef
     password : process.env.DB_PASS,
+    // eslint-disable-next-line no-undef
     port : process.env.DB_PORT
 })
 
-module.exports = db
+const connection = async ()=>{
+    try {
+        await db.connect();
+        console.log(`Database connected`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+module.exports = {db, connection}
