5a2378b5097750dde8df172033a77705781c695f
andrei <andre4acc@gmail.com>
2022-06-15 15:45:23 +0700

    Make it better

--- coffeeland-server/src/config/db.js
+++ coffeeland-server/src/config/db.js
@@ -3,16 +3,21 @@ const psql = require('pg')
 const {Pool} = psql
 
 const db = new Pool({
+    // // eslint-disable-next-line no-undef
+    // user : process.env.DB_USER,
+    // // eslint-disable-next-line no-undef
+    // host : process.env.DB_HOST,
+    // // eslint-disable-next-line no-undef
+    // database : process.env.DB_DATABASE,
+    // // eslint-disable-next-line no-undef
+    // password : process.env.DB_PASS,
+    // // eslint-disable-next-line no-undef
+    // port : process.env.DB_PORT
     // eslint-disable-next-line no-undef
-    user : process.env.DB_USER,
-    // eslint-disable-next-line no-undef
-    host : process.env.DB_HOST,
-    // eslint-disable-next-line no-undef
-    database : process.env.DB_DATABASE,
-    // eslint-disable-next-line no-undef
-    password : process.env.DB_PASS,
-    // eslint-disable-next-line no-undef
-    port : process.env.DB_PORT
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
 })
 
 const connection = async ()=>{
