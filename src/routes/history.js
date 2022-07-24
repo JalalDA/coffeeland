1c8fa0fd9fcfd87fef2df17923260c8261b52741
andrei <andre4acc@gmail.com>
2022-07-24 11:05:33 +0700

    make it better

--- src/routes/history.js
+++ src/routes/history.js
@@ -3,7 +3,7 @@ const Router = express.Router()
 const {verifyToken} = require('../middleware/auth')
 const {getHistoryTransaction, deleteHistoryTransaction} = require('../controllers/history')
 
-Router.get('/', getHistoryTransaction)
+Router.get('/', verifyToken, getHistoryTransaction)
 Router.patch('/delete', deleteHistoryTransaction)
 
 module.exports = Router
